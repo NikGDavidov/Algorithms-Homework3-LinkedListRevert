9a643a900704c567dcb3298a31a79769454fae55
Nikolay_Davydov <skmoscow74@yandex.ru>
2023-01-25 19:42:48 +0300

    homework2

--- LinkedListSample.java
+++ LinkedListSample.java
@@ -1,5 +1,5 @@
 package LinkedListSample;
-
+//Необходимо реализовать метод разворота связного списка (двухсвязного или односвязного на выбор).
 public class LinkedListSample {
     public static void main(String[] args) {
         List list = new List();
@@ -16,89 +16,85 @@ public class LinkedListSample {
     }
 
 }
-     class List{
-        Node head;
-        private class Node{
-            Node next;
-            int value;
-        }
-        void print(){
-            System.out.println();
-            Node node = head;
-            while (node!=null){
-                System.out.print(node.value + " ");
-                node=node.next;
-            }
-        }
-        void addFirst (int value){
-            Node node = new Node();
-            node.value = value;
-            if (head!= null) {
-                node.next=head;
-            }
-            head=node;
-        }
-        void removeFirst(){
-            if (head!=null) head = head.next;
-        }
-        boolean contains(int value){
-            Node node = head;
-            while(node!=null){
-                if (node.value==value) return true;
-                node = node.next;
-            }
-            return false;
-        }
-        void addLast (int value){
-            if (head ==null) {head=new Node(); head.value = value;}
-            Node node = head;
-            while (node.next!=null) {node = node.next;}
-            node.next = new Node();
-            node.next.value = value;
-        }
-        void removeLast (){
-             if (head==null) return;
-             if (head.next ==null){head =null; return;}
-             Node node =head;
-             while (node.next!=null){
-                 if (node.next.next==null){node.next =null;
-                     return;}
+     class List {
+         Node head;
+
+         private class Node {
+             Node next;
+             int value;
+         }
+
+         void print() {
+             System.out.println();
+             Node node = head;
+             while (node != null) {
+                 System.out.print(node.value + " ");
                  node = node.next;
              }
-        }
-        void revert(){
-            Node current = head.next;
-            head.next = null;
-            while (current!=null) {
-                Node next = current.next;
-                current.next = head;
-                head = current;
-                current = next;
-            }
-        }
-        void revers() {
-            if ((head == null) || (head.next == null)) return;
-            Node current = head.next;
-            head.next = null;
-            while (current != null) {
-                Node next = current.next;
-                current.next = head;
-                head = current;
-                current = next;
-            }
-        }
-            void revers1(){
-                Node current = head.next;
-                head.next = null;
-                while (current!=null){
-                    Node next = current.next;
-                    current.next = head;
-                    head = current;
-                    current = next;
-                }
+         }
 
-            }
-}
+         void addFirst(int value) {
+             Node node = new Node();
+             node.value = value;
+             if (head != null) {
+                 node.next = head;
+             }
+             head = node;
+         }
+
+         void removeFirst() {
+             if (head != null) head = head.next;
+         }
+
+         boolean contains(int value) {
+             Node node = head;
+             while (node != null) {
+                 if (node.value == value) return true;
+                 node = node.next;
+             }
+             return false;
+         }
+
+         void addLast(int value) {
+             if (head == null) {
+                 head = new Node();
+                 head.value = value;
+             }
+             Node node = head;
+             while (node.next != null) {
+                 node = node.next;
+             }
+             node.next = new Node();
+             node.next.value = value;
+         }
+
+         void removeLast() {
+             if (head == null) return;
+             if (head.next == null) {
+                 head = null;
+                 return;
+             }
+             Node node = head;
+             while (node.next != null) {
+                 if (node.next.next == null) {
+                     node.next = null;
+                     return;
+                 }
+                 node = node.next;
+             }
+         }
+
+         void revert() {
+             Node current = head.next;
+             head.next = null;
+             while (current != null) {
+                 Node next = current.next;
+                 current.next = head;
+                 head = current;
+                 current = next;
+             }
+         }
+     }
 
 
 
